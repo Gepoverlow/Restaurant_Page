c5690216d9c950d155047c8939b9beaca6c845a2
Gepoverlow <gepeoverload@gmail.com>
2021-09-03 18:42:45 +0200

    homepage function working

--- src/homeModule.js
+++ src/homeModule.js
@@ -20,11 +20,23 @@ function homePage() {
   nav.appendChild(menu);
   nav.appendChild(contact);
 
-  // MAIN
+  ////// MAIN
   let contentMainHome = document.createElement("div");
   contentMainHome.id = "content-main-home";
   content.appendChild(contentMainHome);
-  //
+  //// H1
+  let titleHome = document.createElement("h1");
+  titleHome.id = "title-home";
+  contentMainHome.appendChild(titleHome);
+  titleHome.innerHTML = "Welcome to La Parrilla";
+  //// HOME
+  let contentHome = document.createElement("div");
+  contentHome.id = "content-home";
+  contentMainHome.appendChild(contentHome);
+  // PARAGRAPH
+  let paragraph = document.createElement("p");
+  contentHome.appendChild(paragraph);
+  paragraph.innerHTML =
+    "In Argentina, asado is everything. The factor that brings the family together on Sundays, and friends some night of the week. The Argentine barbecue is a cult, a ritual, something much bigger than a simple meal.";
 }
-
 export { homePage };
